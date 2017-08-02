3eed94d10f16169a2b6c14f61e98740ee7f13319
Filip Kinský <filip@filovo.net>
2017-08-02 16:42:43 +0200

    removed createJSModules method (RN 0.47 breaking change)
    see https://github.com/facebook/react-native/releases/tag/v0.47.0 for explanation

--- android/lib/src/main/java/com/cube/geofencing/RNRegionMonitorPackage.java
+++ android/lib/src/main/java/com/cube/geofencing/RNRegionMonitorPackage.java
@@ -18,12 +18,6 @@ public class RNRegionMonitorPackage implements ReactPackage
 	}
 
 	@Override
-	public List<Class<? extends JavaScriptModule>> createJSModules()
-	{
-		return Collections.emptyList();
-	}
-
-	@Override
 	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext)
 	{
 		return Collections.emptyList();
